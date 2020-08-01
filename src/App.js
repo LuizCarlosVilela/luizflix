5da124f303ee28f2ec08a9ffb5c15c29f051769d
LuizCarlosVilela <luizcarlosvilela@outlook.com.br>
2020-08-01 00:48:00 -0300

    Fazendo banner inicial e colocando os dados para funcionar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
 import Menu from './components/Menu';
+import dadosIniciais from './data/dados_iniciais.json';
+
+import BannerMain from './components/BannerMain';
+import Carrousel from './components/Carousel';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Menu />
-      
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"O que é Front-end? Trabalhando na área"}
+      />
+
     </div>
   );
 }
