d6663f3f28898dd5e4748667e5495fe2247975b2
LuizCarlosVilela <luizcarlosvilela@outlook.com.br>
2020-08-03 12:08:02 -0300

    Insirindo rotas no site, para cadastro e erro 404

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+const Pagina404 = () => <div>404</div> ;
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={App} exact />
+      <Route path="/cadastro/video" component={ () => <h1>Deus é mais</h1>} exact />
+
+      <Route component={ Pagina404 } />
+    </Switch>
+  </BrowserRouter>,
+
   document.getElementById('root')
 );
 
